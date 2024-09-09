aba9c05c9591cc019b151fc0dd538f88f07a9e54
Rajesh Keerthi <raju.kk043@gmail.com>
2024-09-09 23:43:53 +0530

    removed set headers

--- index.js
+++ index.js
@@ -32,25 +32,6 @@ app.use(bodyparser.json());
 // set view engine
 app.set('view engine', 'ejs');
 // app.set("views", path.resolve(__dirname, 'views/ejs'))
-app.use((req, res, next) => {
-    const origin = req.get('referer');
-    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-    if (isWhitelisted) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-    }
-    // Pass to next layer of middleware
-    if (req.method === 'OPTIONS') res.sendStatus(200);
-    else next();
-  });
-  
-  const setContext = (req, res, next) => {
-    if (!req.context) req.context = {};
-    next();
-  };
-  app.use(setContext);
 
 
 // load assets
